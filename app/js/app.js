5e7b6ce56a2d53d669411a94f3c462f8bd53056f
mandriva19 <97590591+mandriva19@users.noreply.github.com>
2023-06-07 05:13:18 +0400

    fxx

--- app/js/app.js
+++ app/js/app.js
@@ -1,50 +1,52 @@
-//get navbar-clicker btn
+// Get elements
 const navbarClick = document.querySelector(".navbar-click");
-//get navbar-overlay
 const navbarOverlay = document.querySelector(".navbar-overlay");
-//get navbar items menu
 const navbarItems = document.querySelector(".navbar-items");
-//get body element
 const body = document.querySelector("body");
 
-// function toggles between clicked/not clicked for navbar
+// Toggle function for navbar click
 const toggleNavClick = () => {
   navbarClick.classList.toggle("is-clicked");
   navbarItems.classList.toggle("is-hidden");
+  navbarItems.classList.add("animate__zoomIn");
 };
 
-//hide navbar-items when user clicks outside the navbar-items
+// Event listener for hiding navbar items when clicked outside
 navbarOverlay.addEventListener("click", (event) => {
   if (event.target === navbarOverlay) {
     navbarOverlay.classList.add("is-hidden");
     navbarItems.classList.add("is-hidden");
-    body.style.overflow = "auto";
     navbarClick.classList.remove("is-clicked");
   }
 });
 
-//check if navbar items is open and disable scroll
-const scrollDisable = () => {
-  if (navbarItems.classList.contains("is-hidden")) {
-    body.style.overflow = "auto";
-  } else {
-    body.style.overflow = "hidden";
-  }
-};
-
-//this is general function that shows/hide overlay/nav-items and adds classes for animations
+// Function for showing/hiding overlay
 const navbarClickFunction = () => {
   if (navbarOverlay.classList.contains("is-hidden")) {
     navbarOverlay.classList.remove("is-hidden");
-    navbarOverlay.classList.add("animate__zoomIn");
+    // navbarOverlay.classList.add("animate__zoomIn");
   } else {
     navbarOverlay.classList.add("is-hidden");
   }
 };
 
-//event-listener for navbar-click. it runs functions that are declared above
-navbarClick.addEventListener("click", function () {
+// Close navbarItems when user scrolls bellow it
+function handleScroll() {
+  const scrollPosition =
+    window.pageYOffset || document.documentElement.scrollTop;
+
+  if (scrollPosition >= 350) {
+    navbarOverlay.classList.add("is-hidden");
+    navbarItems.classList.add("is-hidden");
+    navbarClick.classList.remove("is-clicked");
+  }
+}
+
+// Add scroll event listener to the window
+window.addEventListener("scroll", handleScroll);
+
+// Event listener for navbar click
+navbarClick.addEventListener("click", () => {
   toggleNavClick();
   navbarClickFunction();
-  scrollDisable();
 });
---
+++ read-me.md
@@ -0,0 +1,9 @@
+### This is EasyBank Landing Page Challenge From Front-End-Mentor
+
+I used mobile first approach to built this project for 3 different viewports:
+
+mobile,
+tablet,
+desktop+
+
+I tried to be consistent with css
